ae70641b200365e311295cd65e8027b02096c81a
evildethow <evildethow@gmail.com>
2018-06-18 18:04:08 +1000

    [HOTFIX] CE-1812 - Syntax error fixes

--- install.sh
+++ install.sh
@@ -4,15 +4,17 @@
 
 readonly GEOFFREY_HOME="${HOME}/geoffrey"
 readonly GEOFFREY_BIN="${GEOFFREY_HOME}/bin"
+readonly GEOFFREY_EXEC="${GEOFFREY_BIN}/geoffrey"
 readonly REMOTE_BASE_URL="https://github.com/cloudbees/support-required-data-geoffrey"
 reeaonly REMOTE_REPO="${REMOTE_BASE_URL}/raw/master"
 
 # Download
 
-mkdir -p "${GEOFFREY_BIN}" curl -L -o "${GEOFFREY_BIN}/geoffrey" "${REMOTE_REPO}/bin/geoffrey"
+mkdir -p "${GEOFFREY_BIN}" curl -L -o "${GEOFFREY_EXEC}" "${REMOTE_REPO}/bin/geoffrey"
 
 # Install
 
+chmod +x "${GEOFFREY_EXEC}"
 export PATH="${PATH}:${GEOFFREY_BIN}"
 
 echo "Installed to ${GEOFFREY_BIN}"
